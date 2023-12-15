6af6f185452cf088ff85a494475c2c48050ddefa
abhi3700 <dalvath3700@gmail.com>
2023-12-15 23:53:02 +0530

    is-user-verified script modified with SemaphoreEthers

--- scripts/is-user-verified.ts
+++ scripts/is-user-verified.ts
@@ -3,13 +3,17 @@
  * The script runs the check off-chain. Just collecting some info from the Nova chain.
  * So, no gas fees for this.
  *
- * FIXME: This script is not working as expected. This is because the Semaphore subgraph
+ * FIXME: [SemaphoreSubgraph] This script is not working as expected. This is because the Semaphore subgraph
  *      may not be configured for Nova network.
+ *      Solution: Use `SemaphoreEthers` instead of `SemaphoreSubgraph`.
+ *
+ * FIXME: [SemaphoreEthers] This script sometimes working, else throws error: "query timeout of 10 seconds exceeded".
+ *          Solution: Increase the timeout.
  */
 
-import { SemaphoreSubgraph } from "@semaphore-protocol/data";
+import { SemaphoreSubgraph, SemaphoreEthers } from "@semaphore-protocol/data";
 import { ethers } from "hardhat";
-import { readContractAddress } from "./utils";
+import { readContractAddresses } from "./utils";
 import { Identity } from "@semaphore-protocol/identity";
 import { DidRegistry } from "../build/typechain";
 
@@ -35,14 +39,25 @@ async function main() {
 
     // after running `$ yarn hardhat deploy --network nova`, you can get the DID Registry address
     // from "../deployed-subspace-nova.json".
-    const didRegistryAddress: string = readContractAddress(CONFIG_FILE_PATH);
+    const didRegistryAddress: string =
+        readContractAddresses(CONFIG_FILE_PATH).DidRegistry;
+    const semaphoreAddress: string =
+        readContractAddresses(CONFIG_FILE_PATH).Semaphore;
 
     // client
     const provider = new ethers.providers.JsonRpcProvider(NOVA_RPC_URL);
 
-    const semaphoreSubgraph = new SemaphoreSubgraph(
-        "https://nova.squid.gemini-3g.subspace.network/graphql"
-    );
+    // Using `SemaphoreSubgraph`
+    // const semaphoreSubgraph = new SemaphoreSubgraph(
+    //     "https://nova.squid.gemini-3g.subspace.network/graphql"
+    // );
+
+    // Using `SemaphoreEthers`
+    const semaphoreEthers = new SemaphoreEthers(NOVA_RPC_URL, {
+        address: semaphoreAddress,
+        // TODO: decide this? It has to be the block where `semaphoreAddress` was used to deploy the contract.
+        startBlock: 362702,
+    });
 
     const didRegistryContract = new ethers.Contract(
         didRegistryAddress,
@@ -58,10 +73,18 @@ async function main() {
     let user = new Identity();
     const identityCommitment = user.commitment;
 
-    let isMember = await semaphoreSubgraph.isGroupMember(
-        groupId.toString(),
-        identityCommitment.toString()
-    );
+    // using `SemaphoreSubgraph`
+    // let isMember = await semaphoreSubgraph.isGroupMember(
+    //     groupId.toString(),
+    //     identityCommitment.toString()
+    // );
+
+    // using `SemaphoreEthers`
+    // a. get the group members
+    const members = await semaphoreEthers.getGroupMembers(groupId.toString());
+    // b. check if the user commitment is present in the group
+    let isMember = members.includes(identityCommitment.toString());
+
     console.log(`Is user ${identityCommitment} in group? ${isMember}`);
 }
 
