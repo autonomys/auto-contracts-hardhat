77f0873226ec8bcde548cb480ec4f9b3c5c778ea
Natacha De la Rosa <delarosa.natacha@gmail.com>
2023-12-20 18:46:53 -0400

    Update is-user-verified.ts

--- scripts/did/is-user-verified.ts
+++ scripts/did/is-user-verified.ts
@@ -141,7 +141,7 @@ async function main() {
     const identityCommitment = user.commitment;
 
     // Approach-1: ❌ (Request failed with status code 400)
-    // const isMember = await approach1(groupId, identityCommitment);
+    const isMember = await approach1(groupId, identityCommitment);
 
     // Approach-2: ✅ 14s (takes more time than Approach-3)
     // const isMember = await approach2(
@@ -152,11 +152,11 @@ async function main() {
     // );
 
     // Approach-3: ✅ 8s
-    const isMember = await approach3(
-        didRegistryContract,
-        deployedBlockNumber,
-        identityCommitment
-    );
+    // const isMember = await approach3(
+    //     didRegistryContract,
+    //     deployedBlockNumber,
+    //     identityCommitment
+    // );
 
     console.log(`Is user with \'${identityCommitment}\' in group? ${isMember}`);
 }
